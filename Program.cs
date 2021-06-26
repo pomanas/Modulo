cb151f70be35119fb727a513bea33242944a8fa6
pomanas <slawomir.pomana@gmail.com>
2021-06-26 18:56:40 +0200

    first commit Modulo

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            var number = GetNumber();
+            var modulo = number % 2;
+            if (modulo == 0)
+            {
+                Console.WriteLine("Twoja liczba jest parzysta.");
+            }
+            else
+            {
+                Console.WriteLine("Twoja liczba jest nieparzysta.");
+            }
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                {
+                    Console.WriteLine("Podałeś nieprawidłowe dane. Spróbuj ponownie.");
+                        continue;
+                }
+                    return userNumber;
+            }
+        }
+    }
+}
